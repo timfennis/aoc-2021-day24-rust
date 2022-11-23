3cc7b0b01a5b99e470d893cd55f9acdc0c42d0b3
Tim Fennis <tim.fennis@nos.nl>
2022-11-23 19:38:57 +0100

    Simplify statements

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn solve_block(w: i64, z: i64, div: i64, offset_1: i64, offset_2: i64) -> i64 {
     let x = z % 26 + offset_1;
         
     return if x == w {
-        (z / div) * 1 + 0
+        z / div  
     } else {
         (z / div) * 26 + w + offset_2
     }
