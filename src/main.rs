f323517c2a110aa015a1161c302f3b4da740c070
Tim Fennis <tim.fennis@nos.nl>
2022-11-23 22:01:40 +0100

    add final solver stage

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,106 @@
-use std::ops::Range;
-use std::time::{SystemTime, UNIX_EPOCH};
-
 fn main() {
-    let range: Range<i64> = 1..99999999999999;
+    let solutions = find_all_solutions();
 
-    let start = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
+    for solution in solutions {
+        let is_valid = calculate_z(&solution) == 0;
 
-    for n in range.rev() {
-        let input = n.to_string();
-
-        if !input.contains("0") {
-            let solution = solve(&input);
-
-            if solution == 0 {
-                println!("Found the magic input: {input}");
-            }
-        }
-
-        if n % 1000000 == 0 {
-            let now = SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .unwrap()
-                .as_secs();
-
-            println!("Currently at {n}");
-
-            if now - start > 30 {
-                println!("Done running for 30 seconds");
-                break;
-            }
-        }
+        println!("Solution: {solution} is_valid={is_valid}");
     }
 }
 
-fn solve_block(input: i64, stack: i64, div: i64, offset_1: i64, offset_2: i64) -> i64 {
-    let peek = stack % 26 + offset_1;
-        
-    return if peek == input {
-        // if (div == 1) 
-        //     stack
-        // if (div == 26)
-        //     stack / 26
-        stack / div  
-    } else {
-        // if (div == 1)
-        //     stack * 26 + (input + offset_2)
-        // if (div == 26) 
-        //     stack + (input + offset_2)
-        (stack / div) * 26 + input + offset_2
+fn find_all_solutions() -> Vec<String> {
+    let mut solutions = Vec::new();
+
+    for (n1, n14) in solve_pair(13, 14, -14, 3) {
+        for (n2, n13) in solve_pair(12, 8, -2, -13) {
+            for (n3, n4) in solve_pair(11, 5, 0, 4) {
+                for (n5, n6) in solve_pair(15, 10, -13, 13) {
+                    for (n7, n8) in solve_pair(10, 16, -9, 5) {
+                        for (n9, n12) in solve_pair(11, 6, -3, 7) {
+                            for (n10, n11) in solve_pair(13, 13, -14, 6) {
+                                let solution = format!(
+                                    "{n1}{n2}{n3}{n4}{n5}{n6}{n7}{n8}{n9}{n10}{n11}{n12}{n13}{n14}"
+                                );
+
+                                solutions.push(solution);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
+
+    return solutions;
 }
 
-fn solve(input: &str) -> i64 {
+fn calculate_z(input: &str) -> i64 {
     let mut input: Vec<i64> = input
         .chars()
         .map(|c| c.to_digit(10).unwrap() as i64)
         .rev()
         .collect();
 
-    let z = solve_block(input.pop().unwrap(), 0, 1, 13, 14);
-    let z = solve_block(input.pop().unwrap(), z, 1, 12, 8);
-    let z = solve_block(input.pop().unwrap(), z, 1, 11, 5);
-    let z = solve_block(input.pop().unwrap(), z, 26, 0, 4);
-    let z = solve_block(input.pop().unwrap(), z, 1, 15, 10);
-    let z = solve_block(input.pop().unwrap(), z, 26, -13, 13);
-    let z = solve_block(input.pop().unwrap(), z, 1, 10, 16);
-    let z = solve_block(input.pop().unwrap(), z, 26, -9, 5);
-    let z = solve_block(input.pop().unwrap(), z, 1, 11, 6);
-    let z = solve_block(input.pop().unwrap(), z, 1, 13, 13);
-    let z = solve_block(input.pop().unwrap(), z, 26, -14, 6);
-    let z = solve_block(input.pop().unwrap(), z, 26, -3, 7);
-    let z = solve_block(input.pop().unwrap(), z, 26, -2, 13);
-    let z = solve_block(input.pop().unwrap(), z, 26, -14, 3);
+    let z = calculate_z_block(input.pop().unwrap(), 0, 1, 13, 14);   // push 1
+    let z = calculate_z_block(input.pop().unwrap(), z, 1, 12, 8);    // push 2
+    let z = calculate_z_block(input.pop().unwrap(), z, 1, 11, 5);    // push 3
+    let z = calculate_z_block(input.pop().unwrap(), z, 26, 0, 4);    // pop 3
+    let z = calculate_z_block(input.pop().unwrap(), z, 1, 15, 10);   // push 4
+    let z = calculate_z_block(input.pop().unwrap(), z, 26, -13, 13); // pop 4
+    let z = calculate_z_block(input.pop().unwrap(), z, 1, 10, 16);   // push 5
+    let z = calculate_z_block(input.pop().unwrap(), z, 26, -9, 5);   // pop 5
+    let z = calculate_z_block(input.pop().unwrap(), z, 1, 11, 6);    // push 6
+    let z = calculate_z_block(input.pop().unwrap(), z, 1, 13, 13);   // push 7
+    let z = calculate_z_block(input.pop().unwrap(), z, 26, -14, 6);  // pop 7
+    let z = calculate_z_block(input.pop().unwrap(), z, 26, -3, 7);   // pop 6
+    let z = calculate_z_block(input.pop().unwrap(), z, 26, -2, 13);  // pop 2
+    let z = calculate_z_block(input.pop().unwrap(), z, 26, -14, 3);  // pop 1
 
     return z;
 }
 
+fn calculate_z_block(input: i64, stack: i64, div: i64, offset_1: i64, offset_2: i64) -> i64 {
+    let peek = stack % 26 + offset_1;
+
+    return if peek == input {
+        stack / div
+    } else {
+        stack / div * 26 + input + offset_2
+    };
+}
+
+fn solve_pair(
+    push_offset_1: i64,
+    push_offset_2: i64,
+    pop_offset_1: i64,
+    pop_offset_2: i64,
+) -> Vec<(i64, i64)> {
+    let mut options = Vec::new();
+
+    for i in 1..=9i64 {
+        let z = calculate_z_block(i, 0, 1, push_offset_1, push_offset_2);
+
+        for o in 1..=9i64 {
+            let z = calculate_z_block(o, z, 26, pop_offset_1, pop_offset_2);
+
+            if z == 0 {
+                options.push((i, o));
+            }
+        }
+    }
+
+    return options;
+}
 #[cfg(test)]
 mod tests {
+    use crate::{calculate_z, find_all_solutions};
+
     #[test]
-    fn it_works() {
-        let result = crate::solve("93499629698999");
-        assert_eq!(result, 0);
+    fn all_solutions_are_valid() {
+        let solutions = find_all_solutions();
+
+        for solution in solutions {
+            assert_eq!(calculate_z(&solution), 0);
+        }
     }
 }
