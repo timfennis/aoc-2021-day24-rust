8711293d9be1b0f1f614f6f84048806096878e48
Tim Fennis <tim.fennis@nos.nl>
2022-11-23 17:52:01 +0100

    generalize function and replace all blocks

--- src/main.rs
+++ src/main.rs
@@ -36,12 +36,12 @@ fn main() {
     }
 }
 
-fn solve_block_1(w: i64, z: i64) -> i64 {
+fn solve_block(w: i64, z: i64, div: i64, offset_1: i64, offset_2: i64) -> i64 {
     let x = 0;
     let x = x + z;
     let x = x % 26;
-    let z = z / 1;
-    let x = x + 13;
+    let z = z / div;
+    let x = x + offset_1;
     let x = if x == w { 1 } else { 0 };
     let x = if x == 0 { 1 } else { 0 };
     let y = 0;
@@ -51,7 +51,7 @@ fn solve_block_1(w: i64, z: i64) -> i64 {
     let z = z * y;
     let y = 0;
     let y = y + w;
-    let y = y + 14;
+    let y = y + offset_2;
     let y = y * x;
     let z = z + y;
 
@@ -65,265 +65,21 @@ fn solve(input: &str) -> i64 {
         .rev()
         .collect();
 
-    // Block 1
-    let z = solve_block_1(input.pop().unwrap(), 0);
-
-    // Block 2
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 1;
-    let x = x + 12;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 8;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 1;
-    let x = x + 11;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 5;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 26;
-    let x = x + 0;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 4;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 1;
-    let x = x + 15;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 10;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 26;
-    let x = x + -13;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 13;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 1;
-    let x = x + 10;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 16;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 26;
-    let x = x + -9;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 5;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 1;
-    let x = x + 11;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 6;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 1;
-    let x = x + 13;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 13;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 26;
-    let x = x + -14;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 6;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 26;
-    let x = x + -3;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 7;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 26;
-    let x = x + -2;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 13;
-    let y = y * x;
-    let z = z + y;
-
-    let w = input.pop().unwrap();
-    let x = 0;
-    let x = x + z;
-    let x = x % 26;
-    let z = z / 26;
-    let x = x + -14;
-    let x = if x == w { 1 } else { 0 };
-    let x = if x == 0 { 1 } else { 0 };
-    let y = 0;
-    let y = y + 25;
-    let y = y * x;
-    let y = y + 1;
-    let z = z * y;
-    let y = 0;
-    let y = y + w;
-    let y = y + 3;
-    let y = y * x;
-    let z = z + y;
+    let z = solve_block(input.pop().unwrap(), 0, 1, 13, 14);
+    let z = solve_block(input.pop().unwrap(), z, 1, 25, 8);
+    let z = solve_block(input.pop().unwrap(), z, 1, 12, 8);
+    let z = solve_block(input.pop().unwrap(), z, 1, 11, 5);
+    let z = solve_block(input.pop().unwrap(), z, 26, 0, 4);
+    let z = solve_block(input.pop().unwrap(), z, 1, 15, 10);
+    let z = solve_block(input.pop().unwrap(), z, 26, -13, 13);
+    let z = solve_block(input.pop().unwrap(), z, 1, 10, 16);
+    let z = solve_block(input.pop().unwrap(), z, 26, -9, 5);
+    let z = solve_block(input.pop().unwrap(), z, 1, 11, 6);
+    let z = solve_block(input.pop().unwrap(), z, 1, 13, 13);
+    let z = solve_block(input.pop().unwrap(), z, 26, -14, 6);
+    let z = solve_block(input.pop().unwrap(), z, 26, -3, 7);
+    let z = solve_block(input.pop().unwrap(), z, 26, -2, 13);
+    let z = solve_block(input.pop().unwrap(), z, 26, -14, 3);
 
     return z;
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = crate::solve("93499629698999");
-        assert_eq!(result, 0);
-    }
 }
